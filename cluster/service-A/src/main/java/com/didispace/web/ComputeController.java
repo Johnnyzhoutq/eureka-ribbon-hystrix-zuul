17363a8bd4118291f0af8d1db45f9a5539959673
Johnnyzhoutq <johnnyzhoutq@gmail.com>
2017-03-02 15:56:30 +0800

    add

--- cluster/service-A/src/main/java/com/didispace/web/ComputeController.java
+++ cluster/service-A/src/main/java/com/didispace/web/ComputeController.java
@@ -2,13 +2,15 @@ package com.didispace.web;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+@RefreshScope
 @RestController
 public class ComputeController {
 
@@ -16,12 +18,20 @@ public class ComputeController {
 
     @Autowired
     private DiscoveryClient client;
-    
+    @Value("${from}")
+    private String from;
     @RequestMapping(value = "/add" ,method = RequestMethod.GET)
     public String add(@RequestParam Integer a, @RequestParam Integer b) {
         ServiceInstance instance = client.getLocalServiceInstance();
         Integer r = a + b;
         logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r );
         return "From Service-A, Result is " + r;
+    }
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getFrom() {
+        return from;
     }
 }
